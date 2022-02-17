206a82f2c1b69e5609b9fc9e855f1ae8ecaa4051
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-17 19:36:58 +0300

    Sayfa Üzerinde Görsel İyileştirmeler (React)

--- src/UserSignupPage.js
+++ src/UserSignupPage.js
@@ -31,30 +31,50 @@ class UserSignupPage extends React.Component {
 
   render() {
     return (
-      <form>
-        <h1>Sign Up</h1>
-        <div>
-          <label>Username</label>
-          <input name="username" onChange={this.onChange} />
-        </div>
-        <div>
-          <label>Display Name</label>
-          <input name="displayName" onChange={this.onChange} />
-        </div>
-        <div>
-          <label>Password</label>
-          <input name="password" onChange={this.onChange} type="password" />
-        </div>
-        <div>
-          <label>Password Confirm</label>
-          <input
-            name="passwordRepeat"
-            onChange={this.onChange}
-            type="password"
-          />
-        </div>
-        <button onClick={this.onClickSignup}>Sign Up</button>
-      </form>
+      <div className="container">
+        <form>
+          <h1 className="text-center">Sign Up</h1>
+          <div className="form-group">
+            <label>Username</label>
+            <input
+              class="form-control"
+              name="username"
+              onChange={this.onChange}
+            />
+          </div>
+          <div>
+            <label>Display Name</label>
+            <input
+              className="form-control"
+              name="displayName"
+              onChange={this.onChange}
+            />
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              className="form-control"
+              name="password"
+              onChange={this.onChange}
+              type="password"
+            />
+          </div>
+          <div>
+            <label>Password Confirm</label>
+            <input
+              className="form-control"
+              name="passwordRepeat"
+              onChange={this.onChange}
+              type="password"
+            />
+          </div>
+          <div className="text-center">
+            <button className="btn btn-primary" onClick={this.onClickSignup}>
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
     );
   }
 }
